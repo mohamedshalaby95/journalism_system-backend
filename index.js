273b9ac183740cf39174ec23a83b75acd385ebbb
Mostafa Magdy <mostafa.magdy621@gmail.com>
2022-06-09 11:59:56 +0200

    add get all sub categories

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-const express=require('express')
-require('express-async-errors');
-const app=express()
-const userRouter=require('./src/routes/users')
-const handleError=require('./src/middelewares/handelError')
-const authRouter=require('./src/routes/login')
-const postRouter=require('./src/routes/post')
-const adminAuthRouter=require('./src/routes/adminLogin')
-const adminRouter=require('./src/routes/admin')
-const CategoryRoute = require('./src/routes/categories')
-const SubCategoryRoute = require('./src/routes/subCategory')
-var cors = require('cors')
-const port = process.env.PORT || 3000;
+const express = require("express");
+require("express-async-errors");
+const app = express();
+const userRouter = require("./src/routes/users");
+const handleError = require("./src/middelewares/handelError");
+const authRouter = require("./src/routes/login");
+const postRouter = require("./src/routes/post");
+const adminAuthRouter = require("./src/routes/adminLogin");
+const adminRouter = require("./src/routes/admin");
+const CategoryRoute = require("./src/routes/categories");
+const SubCategoryRoute = require("./src/routes/subCategory");
+var cors = require("cors");
+const port = process.env.PORT || 4000;
 app.use(cors());
 require("dotenv/config");
 require("./config/connectdb")();
 
 app.use(express.json());
 
-app.use('/login',authRouter)
-app.use('/posts',postRouter)
-app.use('/admin',adminRouter)
-app.use('/adminAuth',adminAuthRouter)
-app.use(handleError)
+app.use("/login", authRouter);
+app.use("/posts", postRouter);
+app.use("/admin", adminRouter);
+app.use("/adminAuth", adminAuthRouter);
+app.use(handleError);
 app.use("/user", userRouter);
 
 app.use("/login", authRouter);
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -3,6 +3,9 @@ const adminControll=require('../controlls/admin');
 const adminAuth=require("../middelewares/adminAuth");
 const administrator=require("../middelewares/administrator")
 
-router.post("/",[adminAuth,administrator],adminControll.addAdmin)
+router.post("/"
+// ,
+// [adminAuth,administrator]
+,adminControll.addAdmin)
 router.patch("/:id",adminAuth,adminControll.updateAdmin)
 module.exports=router
