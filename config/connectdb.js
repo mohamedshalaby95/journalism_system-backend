abaa0b7f4baa69ff53d54d0b5a019e1964d23b91
Mostafa Magdy <mostafa.magdy621@gmail.com>
2022-06-15 11:20:10 +0200

    add mostRecently and mostViewed posts

--- config/connectdb.js
+++ config/connectdb.js
@@ -1,28 +1,26 @@
-const mongoose = require('mongoose');
-require('dotenv/config');
+const mongoose = require("mongoose");
+require("dotenv/config");
 
-module.exports=()=>{
-    
-   
-
- if(`${process.env.NODE_ENV}`==='Production'){
-
-    mongoose.connect("mongodb://localhost:27017/journalism")
-        .then(()=>{ console.log('connect mongodb .....')})
-        .catch((error)=>{ console.log(`could not connect with mongodb ${error}`)})
- }
- else{
-     console.log(process.env.DEV_DB)
-    mongoose.connect("mongodb://localhost:27017/journalism")
-        .then(()=>{ console.log(`connect mongodb on ${process.env.DEV_DB}`)})
-        .catch((error)=>{
-            console.log(`could not connect with mongodb ${error}`)})
-            // process.exit(1)
-           
- }
-
-
-
-
-}
-
+module.exports = () => {
+  if (`${process.env.NODE_ENV}` === "Production") {
+    mongoose
+      .connect("mongodb://localhost:27017/Journalism")
+      .then(() => {
+        console.log("connect mongodb .....");
+      })
+      .catch((error) => {
+        console.log(`could not connect with mongodb ${error}`);
+      });
+  } else {
+    console.log(process.env.DEV_DB);
+    mongoose
+      .connect("mongodb://localhost:27017/Journalism")
+      .then(() => {
+        console.log(`connect mongodb on ${process.env.DEV_DB}`);
+      })
+      .catch((error) => {
+        console.log(`could not connect with mongodb ${error}`);
+      });
+    // process.exit(1)
+  }
+};
