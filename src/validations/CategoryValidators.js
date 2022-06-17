d700f2f8b9e342b202638f10fb28958b9647934d
devahmedkhaledsaleh <dev.ahmed.khaled.saleh@gmail.com>
2022-06-17 16:04:07 +0200

    add description and image to category

--- src/validations/CategoryValidators.js
+++ src/validations/CategoryValidators.js
@@ -3,6 +3,8 @@ const Joi = require("joi");
 module.exports = (body) => {
   const Schema = Joi.object({
     title: Joi.string().min(3).max(30).required(),
+    description: Joi.string(),
+    image: Joi.string(),
     // image: Joi.string().required(),
   });
   return Schema.validate(body);
